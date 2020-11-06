d5f24f51a0352a2738e76de56dc8120b25a044c9
Marcelo Luciano <Marcelo.Perseus@gmail.com>
2020-11-06 15:45:12 -0500

    First Build
    Built the app using electronforge. There is a settings.json file in the resources/app folder where you should specify the ip address of the server you are connecting to. The build folder is too big to put in GitHub though.

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function createWindow () {
   })
 
   win.loadFile('src/index.html')
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
   win.setMenu(null) // Remove the menu
 }
 
